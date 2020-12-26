338d10b98f582d7288320c015eaef995e2313308
mostafa nasser <47909131+mostafanassermohamed@users.noreply.github.com>
2020-12-26 23:37:46 +0200

    add job model

--- job/models.py
+++ job/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Create your models he
+ 
+JOB_TYPE=( 
+
+    ('full time','full time'),
+    ('part time','part time'),
+    
+
+)
+
+class Job(models.Model):        #table
+    title=models.CharField(max_length=100)
+    #location= 
+    #category=models.ForeignKey('Category',on_delete=models.CASCADE)
+    job_type=models.CharField(max_length=15,choices=JOB_TYPE )
+    description=models.TextField(max_length=1000)
+    published_at=models.DateTimeField(auto_now=True)
+    vacancy=models.IntegerField(default=1)
+    sallary=models.IntegerField(default=0)
+    experience=models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.title     
---
+++ outline.md
@@ -0,0 +1,40 @@
+job:
+   -title
+   -location 
+   -job type
+   -description
+   -published at
+   -valancy
+   -salary
+   -category
+   -experience
+   
+
+
+   -apply job 
+   -post job
+
+
+blog:
+      -tittle
+      -description 
+      -created_at
+      -category
+      -tags
+      -auther
+
+      -search
+      -comment
+      -recent posts
+
+
+contact form
+home
+login
+
+
+
+relationship
+   -one to many     (ForignKey)
+   -many to many    (Many to Many)
+   -one to one      (One to One)
