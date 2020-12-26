c743a763c48d0540ccbe690257129a77ccd8aded
mostafa nasser <47909131+mostafanassermohamed@users.noreply.github.com>
2020-12-27 00:47:09 +0200

    add template file

--- job/models.py
+++ job/models.py
@@ -13,14 +13,20 @@ JOB_TYPE=(
 class Job(models.Model):        #table
     title=models.CharField(max_length=100)
     #location= 
-    #category=models.ForeignKey('Category',on_delete=models.CASCADE)
+    
     job_type=models.CharField(max_length=15,choices=JOB_TYPE )
     description=models.TextField(max_length=1000)
     published_at=models.DateTimeField(auto_now=True)
     vacancy=models.IntegerField(default=1)
     sallary=models.IntegerField(default=0)
     experience=models.IntegerField(default=0)
-
+    category=models.ForeignKey('Category',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title     
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=25)
+    def __str__(self):
+        return self.name
